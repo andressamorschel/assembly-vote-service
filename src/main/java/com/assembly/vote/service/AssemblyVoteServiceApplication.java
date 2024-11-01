cd9239a7d5d1a1bb7dae83a88961e7b091781d01
andressaurbano <andressa.urbano@hoamtechnology.com>
2024-11-01 20:51:34 -0300

    feat: create spring application

---
+++ src/main/java/com/assembly/vote/service/AssemblyVoteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assembly.vote.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssemblyVoteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssemblyVoteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assembly/vote/service/AssemblyVoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assembly.vote.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssemblyVoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
